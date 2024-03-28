ac8def8815a979bfc90c56d8bb85fd01b8587b74
Shankar <shankarsundher21@gmail.com>
2024-03-28 21:42:24 +0530

    ecom practice

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const bar = document.getElementById('bar');
+const navbar = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if(bar) {
+    bar.addEventListener('click', () => {
+    navbar.classList.add('show');
+    })
+}
+if(close){
+    close.addEventListener('click', () => {
+navbar.classList.remove('show');
+    })
+}
